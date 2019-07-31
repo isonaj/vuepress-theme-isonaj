a45bdbe3a10a703674de47610e1e43404751c9ad
Anthony <isonaj@gmail.com>
2019-08-01 07:14:59 +1000

    Added Disqus

--- layouts/Post.vue
+++ layouts/Post.vue
@@ -1,8 +1,24 @@
 <template>
   <div>
     <Content />
-    <ClientOnly v-if="$site.themeConfig.disqusShortName">
-      <Disqus shortname="{{$site.themeConfig.disqusShortName}}" />
+
+    <p>Test</p>
+    <p>{{$site.theme }}</p>
+    <p>{{$site.themeConfig.disqus_shortname }}</p>
+
+    <ClientOnly v-if="$site.themeConfig.disqus_shortname">
+      <Disqus :shortname="$site.themeConfig.disqus_shortname" />
     </ClientOnly>
   </div>
 </template>
+
+<script>
+  import Disqus from 'vuepress-plugin-disqus';
+  
+  export default {
+    name: 'Post',
+    components: {
+      Disqus
+    }
+  }
+</script>
