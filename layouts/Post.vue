537d8dd21b998611f36854fb4be95341b04aeaaa
Anthony <isonaj@gmail.com>
2019-08-02 09:28:09 +1000

    Fixed disqus component

--- layouts/Post.vue
+++ layouts/Post.vue
@@ -2,30 +2,17 @@
   <div>
     <Content />
 
-    <p>Test</p>
-    <p>{{ $site.themeConfig.disqus_shortname }}</p>
-    <p>{{ $themeConfig.disqus_shortname }}</p>
-
-    <p>{{ disqusIdentifier }}</p>
-    <p>{{ disqusUrl }}</p>
-
     <div class="disqus">
       <ClientOnly>
-        <Disqus class="mt-30" :shortname="$themeConfig.disqus_shortname" />
+        <disqus :shortname="$themeConfig.disqus_shortname" />
       </ClientOnly>
     </div>
   </div>
 </template>
 
 <script>
-  import Disqus from 'vuepress-plugin-disqus';
-  import VueDisqus from 'vue-disqus';
-  
   export default {
     name: 'Post',
-    components: {
-      Disqus, VueDisqus
-    },
     computed: {
       disqusIdentifier () {
         return this.$page.frontmatter.disqus || this.$page.path
